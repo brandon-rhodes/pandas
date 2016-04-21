e8041b49b394f657afe22bcaa97aad840f174508
Brandon Rhodes <brandon@rhodesmill.org>
2016-04-21 11:29:00 -0400

    Make test a bit more efficient

--- pandas/tests/test_multilevel.py
+++ pandas/tests/test_multilevel.py
@@ -1345,14 +1345,12 @@ Thur,Lunch,Yes,51.51,17"""
 
     def test_swaplevel_panel(self):
         panel = Panel({'ItemA': self.frame, 'ItemB': self.frame * 2})
+        expected = panel.copy()
+        expected.major_axis = expected.major_axis.swaplevel()
 
-        for result in (
-                panel.swaplevel(axis='major'),
-                panel.swaplevel(0, axis='major'),
-                panel.swaplevel(0, 1, axis='major'),
-                ):
-            expected = panel.copy()
-            expected.major_axis = expected.major_axis.swaplevel()
+        for result in (panel.swaplevel(axis='major'),
+                       panel.swaplevel(0, axis='major'),
+                       panel.swaplevel(0, 1, axis='major')):
             tm.assert_panel_equal(result, expected)
 
     def test_reorder_levels(self):
